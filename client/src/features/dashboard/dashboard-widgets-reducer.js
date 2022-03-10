9ace1d9e1701f60fc3a4f82ac1e49898d5f85812
nick <root@mozaic.com>
2022-03-10 12:38:38 -0500

    custom sharkdao onboarding app

--- client/src/features/dashboard/dashboard-widgets-reducer.js
+++ client/src/features/dashboard/dashboard-widgets-reducer.js
@@ -8,7 +8,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const dashboardWidgets = createSlice({
   name: 'dashboardWidgets',
   initialState: {
-    installedWidgets:[],
+    installedWidgets: [],
     installableWidgets: [],
     visibleWidgets: []
   },
@@ -38,10 +38,13 @@ export const dashboardWidgets = createSlice({
 export const { setInstalledWidgets, setInstallableWidgets, setVisibleWidgets, setNotification } = dashboardWidgets.actions;
 
 
+
+
 export const populateInitialWidgets = (ens) => async (dispatch, getState, axios) => {
 
   var installed = await axios.get('/dashboardWidgets/' + ens)
   // set initial notification status to 0
+
   installed.data.widgets.map(el => el['notify'] = 0)
   dispatch(setInstalledWidgets(installed.data.widgets))
 
@@ -54,7 +57,7 @@ export const populateInitialWidgets = (ens) => async (dispatch, getState, axios)
 export const populateVisibleWidgets = () => async (dispatch, getState) => {
 
   const { dashboardWidgets, gatekeeperRules } = getState();
-  
+
   // rule results stores {rule_id: balance}
   // widget gatekeeper_rules store the contract definition and the thresholds. 
   // for each gatekeeper rule of each widget, we just check that rule result balance > threshold
@@ -70,25 +73,25 @@ export const populateVisibleWidgets = () => async (dispatch, getState) => {
 
 
 
-    // check the gatekeeper configured for each widget
-    dashboardWidgets.installedWidgets.map(function(widget){
-      
-      if(Object.keys(widget.gatekeeper_rules).length == 0){
-        // no rules set. add all to filtered widgets
-        filteredWidgets.push(widget);
-      }
-      else{
-        for (const [key, value] of Object.entries(widget.gatekeeper_rules)){
-          if(gatekeeperRules.ruleResults[key] >= value){
-            filteredWidgets.push(widget)
-            break;
-          }
-        }
-     }
-      })
+  // check the gatekeeper configured for each widget
+  dashboardWidgets.installedWidgets.map(function (widget) {
 
-    dispatch(setVisibleWidgets(filteredWidgets))
-    
+    if (Object.keys(widget.gatekeeper_rules).length == 0) {
+      // no rules set. add all to filtered widgets
+      filteredWidgets.push(widget);
+    }
+    else {
+      for (const [key, value] of Object.entries(widget.gatekeeper_rules)) {
+        if (gatekeeperRules.ruleResults[key] >= value) {
+          filteredWidgets.push(widget)
+          break;
+        }
+      }
+    }
+  })
+
+  dispatch(setVisibleWidgets(filteredWidgets))
+
 
 }
 
@@ -100,9 +103,9 @@ export const updateWidgets = (condition, widget) => async (dispatch, getState) =
 
   const { dashboardWidgets } = getState();
 
-  if(condition == 0){
+  if (condition == 0) {
     // remove from installedWidgets
-    const installedWidgetsApplyRemoved = dashboardWidgets.installedWidgets.filter(function(e){
+    const installedWidgetsApplyRemoved = dashboardWidgets.installedWidgets.filter(function (e) {
       return e.name != widget.name;
     })
     dispatch(setInstalledWidgets(installedWidgetsApplyRemoved))
@@ -113,9 +116,9 @@ export const updateWidgets = (condition, widget) => async (dispatch, getState) =
 
 
   }
-  else if(condition == 1){
+  else if (condition == 1) {
     //remove from installableWidgets
-    const installableWidgetsApplyRemoved = dashboardWidgets.installableWidgets.filter(function(e){
+    const installableWidgetsApplyRemoved = dashboardWidgets.installableWidgets.filter(function (e) {
       return e.name != widget.name;
     })
     dispatch(setInstallableWidgets(installableWidgetsApplyRemoved))
@@ -137,10 +140,10 @@ export const updateWidgetMetadata = (widget, metadata) => async (dispatch, getSt
   let widgetsCopy = JSON.parse(JSON.stringify(dashboardWidgets.installedWidgets));
 
 
-  for (var i in widgetsCopy){
-    if(widgetsCopy[i].name == widget){
+  for (var i in widgetsCopy) {
+    if (widgetsCopy[i].name == widget) {
       widgetsCopy[i].metadata = metadata
-      break;      
+      break;
     }
   }
 
@@ -154,10 +157,10 @@ export const updateWidgetGatekeeper = (widget, gk_rules) => async (dispatch, get
   let widgetsCopy = JSON.parse(JSON.stringify(dashboardWidgets.installedWidgets));
 
 
-  for (var i in widgetsCopy){
-    if(widgetsCopy[i].name == widget){
+  for (var i in widgetsCopy) {
+    if (widgetsCopy[i].name == widget) {
       widgetsCopy[i].gatekeeper_rules = gk_rules;
-      break;      
+      break;
     }
   }
 
@@ -169,8 +172,8 @@ export const setWidgetNotification = (widgetName) => async (dispatch, getState) 
 
   const { dashboardWidgets } = getState();
 
-  for(var i in dashboardWidgets.visibleWidgets){
-    if(dashboardWidgets.visibleWidgets[i].name == widgetName){
+  for (var i in dashboardWidgets.visibleWidgets) {
+    if (dashboardWidgets.visibleWidgets[i].name == widgetName) {
       dispatch(setNotification(i))
       break;
     }
