3aa05f2a82a8358d55ae36e7f9993e12a07441b2
nick <nick.m.dodson@gmail.com>
2022-06-16 11:19:15 -0400

    editor changes

--- client/src/features/creator-contests/components/prompt_builder/editor_tools.js
+++ client/src/features/creator-contests/components/prompt_builder/editor_tools.js
@@ -15,29 +15,30 @@ import Delimiter from '@editorjs/delimiter'
 import InlineCode from '@editorjs/inline-code'
 import SimpleImage from '@editorjs/simple-image'
 
+
 export const EDITOR_JS_TOOLS = {
-  // NOTE: Paragraph is default tool. Declare only when you want to change paragraph option.
-  // paragraph: Paragraph,
-  embed: Embed,
-  table: Table,
-  list: List,
-  warning: Warning,
-  code: Code,
-  linkTool: LinkTool,
-  image: {
-      class: Image,
-      config: {
-          endpoints: {
-              byFile: 'https://localhost:3000/image/upload_img',
-          }
-      }
-  },
-  raw: Raw,
-  header: Header,
-  quote: Quote,
-  marker: Marker,
-  checklist: CheckList,
-  delimiter: Delimiter,
-  inlineCode: InlineCode,
-  simpleImage: SimpleImage,
+    // NOTE: Paragraph is default tool. Declare only when you want to change paragraph option.
+    // paragraph: Paragraph,
+    embed: Embed,
+    table: Table,
+    list: List,
+    warning: Warning,
+    code: Code,
+    linkTool: LinkTool,
+    image: {
+        class: Image,
+        config: {
+            endpoints: {
+                byFile: 'https://localhost:3000/image/upload_img',
+            }
+        }
+    },
+    raw: Raw,
+    header: Header,
+    quote: Quote,
+    marker: Marker,
+    checklist: CheckList,
+    delimiter: Delimiter,
+    inlineCode: InlineCode,
+    simpleImage: SimpleImage,
 }
