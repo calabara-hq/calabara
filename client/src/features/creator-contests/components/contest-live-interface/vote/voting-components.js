74a77381a0ff0dfe0852323097079dc526e3ea95
nick <nick.m.dodson@gmail.com>
2022-08-15 14:11:05 -0400

    voting engine basic display

--- client/src/features/creator-contests/components/contest-live-interface/vote/voting-components.js
+++ client/src/features/creator-contests/components/contest-live-interface/vote/voting-components.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { calculateVotingPower } from "../../../../hooks/useVotingEngine";
+import { useEffect, useMemo, useState } from "react";
+import useVotingEngine from "../../../../hooks/useVotingEngine";
 import styled from "styled-components";
 
 
@@ -17,21 +17,22 @@ const RetractVotesButton = styled.button`
 
 function SubmissionVotingBox({ }) {
     const [votes, setVotes] = useState(null);
-    const [votingPower, setVotingPower] = useState(0);
+    const { votingPower, castVote, retractVotes, exceedVotingPowerError, setExceedVotingPowerError } = useVotingEngine();
 
-    useEffect(() => {
-        setVotingPower(calculateVotingPower(0x1))
-    }, [])
+    console.log('render voting box')
 
     const updateInput = (e) => {
+        if(exceedVotingPowerError) setExceedVotingPowerError(false);
         setVotes(e.target.value)
     }
 
     return (
         <div>
-            <VoteInput value={votes} onChange={updateInput} placeholder="votes"></VoteInput>
-            <CastVotesButton>cast votes</CastVotesButton>
-            <RetractVotesButton>retract all votes</RetractVotesButton>
+            <VoteInput type="number" value={votes} onChange={updateInput} placeholder="votes"></VoteInput>
+            <CastVotesButton onClick={() => castVote(votes)}>cast votes</CastVotesButton>
+            <RetractVotesButton onClick={() => retractVotes()}>retract all votes</RetractVotesButton>
+            <p>voting power: {votingPower}</p>
+            {exceedVotingPowerError && <b><p>amount exceeds available voting power</p></b>}
         </div>
     )
 }
--- client/src/features/hooks/useVotingEngine.js
+++ client/src/features/hooks/useVotingEngine.js
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import { useSelector, useDispatch } from "react-redux"
+import { useState, useMemo, useEffect } from 'react'
+
 
 /**
  * 
@@ -14,11 +14,48 @@ import { useSelector, useDispatch } from "react-redux"
  * 
  */
 
+/*
+*   control the voting power state and actions associated with voting
+*/
 
 const calculateVotingPower = (strategy, wallet, contest_hash) => {
     if (strategy === 0x1) return 20
     else if (strategy === 0x2) return 50
-
 }
 
-export { calculateVotingPower }
+
+
+
+export default function useVotingEngine() {
+    const [votingPower, setVotingPower] = useState(0);
+    const [exceedVotingPowerError, setExceedVotingPowerError] = useState(false);
+
+    useEffect(() => {
+        setVotingPower(calculateVotingPower(0x1))
+    }, [])
+
+
+    const castVote = (numVotes) => {
+        if (numVotes > votingPower) return setExceedVotingPowerError(true)
+        else {
+            setVotingPower(votingPower - numVotes)
+        }
+    }
+
+    const retractVotes = () => {
+        setVotingPower(calculateVotingPower(0x1))
+    }
+
+    return {
+        votingPower: votingPower,
+        exceedVotingPowerError: exceedVotingPowerError,
+        setExceedVotingPowerError: setExceedVotingPowerError,
+        castVote: async (numVotes) => {
+            return await castVote(numVotes);
+        },
+        retractVotes: async () => {
+            return await retractVotes();
+        }
+    }
+
+}
