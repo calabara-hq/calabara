f0e90dc934a8fde2c3b0bfefb0b48fc33cfe79f4
nick <nick.m.dodson@gmail.com>
2022-11-02 18:14:35 -0400

    thread reducer refactor

--- client/src/features/creator-contests/components/contest_settings/contest-settings.js
+++ client/src/features/creator-contests/components/contest_settings/contest-settings.js
@@ -30,7 +30,7 @@ import { submitterRestrictionsState, voterRestrictionsState } from "./contest_ga
 import { selectIsConnected } from "../../../../app/sessionReducer";
 import axios from "axios";
 import { showNotification } from "../../../notifications/notifications";
-
+import TwitterThreadReducer, {twitter_initial_state} from "../../../reducers/twitter-thread-reducer";
 
 
 
@@ -142,88 +142,6 @@ function reducer(state, action) {
 }
 
 
-
-function twitter_reducer(state, action) {
-    switch (action.type) {
-        case 'update_single':
-            return { ...state, ...action.payload };
-        case 'focus_tweet':
-            return {
-                ...state,
-                focus_tweet: action.payload
-            }
-        case 'add_tweet':
-            return {
-                ...state,
-                tweets: [...state.tweets, { text: "" }],
-                focus_tweet: state.tweets.length
-            }
-        case 'update_tweet_text':
-            return {
-                ...state,
-                tweets: [
-                    ...state.tweets.slice(0, action.payload.index),
-                    { ...state.tweets[action.payload.index], text: action.payload.value },
-                    ...state.tweets.slice(action.payload.index + 1)
-                ]
-            }
-        case 'update_tweet_media_preview':
-            // set the preview blob for quick UI
-            return {
-                ...state,
-                tweets: [
-                    ...state.tweets.slice(0, action.payload.index),
-                    { ...state.tweets[action.payload.index], media: { ...state.tweets[action.payload.index].media, preview: action.payload.value } },
-                    ...state.tweets.slice(action.payload.index + 1)
-                ]
-            }
-
-        case 'update_tweet_media_phase_2':
-            // set the media ID and asset url from server response
-            return {
-                ...state,
-                tweets: [
-                    ...state.tweets.slice(0, action.payload.index),
-                    { ...state.tweets[action.payload.index], media: { ...state.tweets[action.payload.index].media, ...action.payload.value } },
-                    ...state.tweets.slice(action.payload.index + 1)
-                ]
-            }
-
-        case 'delete_tweet_media':
-            return {
-                ...state,
-                tweets: [
-                    ...state.tweets.slice(0, action.payload),
-                    { text: state.tweets[action.payload].text },
-                    ...state.tweets.slice(action.payload + 1)
-                ]
-            }
-        case 'delete_tweet':
-            return {
-                ...state,
-                tweets: [
-                    ...state.tweets.slice(0, action.payload),
-                    ...state.tweets.slice(action.payload + 1)
-                ],
-                focus_tweet: action.payload - 1
-            }
-        case 'reset_state':
-            return twitter_initial_state
-        default:
-            throw new Error();
-    }
-}
-
-const twitter_initial_state = {
-    enabled: false,
-    stage: 0,
-    focus_tweet: 0,
-    tweets: [{ text: "" }],
-    error: null
-}
-
-
-
 const initialPromptData = {
     prompt_heading: '',
     prompt_heading_error: false,
@@ -247,7 +165,7 @@ export default function ContestSettings() {
     const [votingStrategyError, setVotingStrategyError] = useState(false);
     const [promptBuilderData, setPromptBuilderData] = useReducer(reducer, initialPromptData)
     const [simpleInputData, setSimpleInputData] = useReducer(reducer, { anonSubmissions: true, visibleVotes: false, selfVoting: false })
-    const [twitterData, setTwitterData] = useReducer(twitter_reducer, twitter_initial_state)
+    const [twitterData, setTwitterData] = useReducer(TwitterThreadReducer, twitter_initial_state)
 
     const TimeBlockRef = useRef(null)
     const RewardsRef = useRef(null)
